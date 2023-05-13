2ff3ac1bc586c3b23800b2f9da03155943937bb9
nadaroshdy155 <nadaroshdy155@gmail.com>
2023-05-13 14:15:20 +0300

    small addition

--- WebApplication7/Site1.Master.designer.cs
+++ WebApplication7/Site1.Master.designer.cs
@@ -42,6 +42,15 @@ namespace WebApplication7
         protected global::System.Web.UI.WebControls.LinkButton LinkButton4;
 
         /// <summary>
+        /// LinkButton5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton5;
+
+        /// <summary>
         /// LinkButton1 control.
         /// </summary>
         /// <remarks>
